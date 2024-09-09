882da2081ee3484a51cf4b37febcccb4bf0a3143
yash-devop <kamble1234meena@gmail.com>
2024-09-09 20:00:31 +0530

    chore: tailwindcss setup

---
+++ apps/web/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content:[
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    '../../packages/ui/src/components/**/*.{ts,tsx}',
+	],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
