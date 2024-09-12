b173fd8152083e0310e1575258b7a8cadc353e69
yash-devop <kamble1234meena@gmail.com>
2024-09-12 09:39:29 +0530

    chore: testing shadcn input from ui folder

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
-import { Button } from "@club/ui/button";
+import {Input} from "@club/ui";
 
 export default function Home() {
   return (
       <>
         <h1 className="bg-red-400 underline">HELLO WORLD</h1>
+        <Input placeholder="FINALLY" type="email" />
       </>
   );
 }
