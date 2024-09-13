5f9f7d98fd1e1b1c3fee3e258ee36b962cd505d5
yash-devop <kamble1234meena@gmail.com>
2024-09-13 17:29:00 +0530

    chore: remove email provider option

--- apps/web/lib/auth.ts
+++ apps/web/lib/auth.ts
@@ -1,40 +1,27 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { NextAuthOptions, SessionStrategy } from "next-auth";
 import CredentialProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
+
+import prisma from '@club/db/client';
 
 const GOOGLE_ID = process.env.GOOGLE_ID || ""
 const GOOGLE_SECRET = process.env.GOOGLE_SECRET || ""
 
-export const NEXT_AUTH_OPTIONS = {
+export const NEXT_AUTH_OPTIONS: NextAuthOptions = {
     providers: [
-        CredentialProvider({
-            name: "Credentials",
-            credentials: {
-                email: {
-                    label: "Email",
-                    placeholder: "Enter Email",
-                    type: "text"
-                },
-                password: {
-                    label: "password",
-                    placeholder: "Enter Password",
-                    type: "password"
-                }
-            },
-            authorize: async (creds) => {          // a function which provides us the credentials as the request object from client. ( or ssr )
-                console.log('creds', creds);
-                return {
-                    id: "user1",
-                    ...creds
-                }
-            }
-        }),
         GoogleProvider({
             clientId: GOOGLE_ID,
             clientSecret: GOOGLE_SECRET,
+            allowDangerousEmailAccountLinking: true
         })
     ],
+    session: {
+        strategy: "jwt" as SessionStrategy
+    },
     // pages: {
     //     "signIn" : '/signin'
     // },
-    secret: process.env.NEXTAUTH_SECRET
+    secret: process.env.NEXTAUTH_SECRET,
+    adapter: PrismaAdapter(prisma)
 };
