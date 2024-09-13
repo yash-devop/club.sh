c3a6eee96c5d4ce674ba8e273cee7e7f5a008f89
yash-devop <kamble1234meena@gmail.com>
2024-09-13 11:10:16 +0530

    chore: next Link AuthLayout

--- apps/web/app/components/AuthLayout.tsx
+++ apps/web/app/components/AuthLayout.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export const AuthLayout = ({
     variant,
     children
@@ -8,16 +10,17 @@ export const AuthLayout = ({
     return (
         <>
             <div className="h-screen w-full">
-                <div className="h-full w-full flex flex-col gap-4 items-center justify-center max-w-3xl mx-auto ">
-                    <div className="max-w-md w-full mx-auto flex flex-col justify-center h-fit rounded-2xl">
-                        <div className="bg-white shadow border border-border p-7 text-center rounded-t-2xl">
+                <div className="h-full w-full flex flex-col gap-4 items-center justify-center max-w-3xl mx-auto">
+                    <div className="max-w-md w-full mx-auto flex flex-col justify-center h-fit md:rounded-2xl">
+                        <div className="bg-white shadow border border-border p-7 text-center md:rounded-t-2xl">
                             <h1 className="font-medium text-lg">{variant === "login" ? "Sign in to your Club Account" : "Create a Club account"}</h1>
                         </div>
-                        <div className="bg-gray-50 border-l border-r border-b shadow flex flex-col p-4 sm:p-4 md:p-6 lg:p-14 gap-4 rounded-b-2xl">
+                        <div className="bg-gray-50 border-l border-r border-b shadow flex flex-col p-4 sm:p-4 md:p-6 lg:p-14 gap-4 md:rounded-b-2xl">
                             {children}
                         </div>
                     </div>
-                    <div className="text-sm">{variant === "login" ? <p>Don't have an account ? <span>Sign up</span></p> : <p>Already have an account ? <span>Sign in</span></p>}</div>
+                    <div className="text-sm">{variant === "login" ? <p>Don't have an account ? <Link className="underline" href={"/signup"}>Sign up</Link></p> : <p>Already have an account ? <Link className="underline" href={"/signin"}>Sign in</Link></p>}</div>
+                
                 </div>
             </div>
         </>
