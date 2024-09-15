becedd0c3a8f9eb0adb871c6fd1d5ee43c8c38ce
yash-devop <kamble1234meena@gmail.com>
2024-09-15 18:43:50 +0530

    add: salt

--- packages/utils/functions/hash-string.ts
+++ packages/utils/functions/hash-string.ts
@@ -1,11 +1,13 @@
-import {createHash} from 'node:crypto'
-export function hashString(str: string){
-    const hash = createHash("sha256");          //sha256 algo.
-    hash.update(str);
+import {createHash , randomBytes} from 'node:crypto';
 
+
+export function hashString(str: string){
+    const hash = createHash("sha256")          //sha256 algo.
+    const salt = randomBytes(10).toString("hex")
+    hash.update(str + salt);
     const hashedString = hash.digest("hex")
 
     console.log('hashed :' , hashedString);
 
-    return hashedString
+    return hashedString;
 }
