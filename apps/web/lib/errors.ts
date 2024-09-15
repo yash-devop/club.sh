82558d5372dc1ec81e423c6c20a6454f47f49085
yash-devop <kamble1234meena@gmail.com>
2024-09-15 18:13:53 +0530

    add: prisma error handler

--- apps/web/lib/errors.ts
+++ apps/web/lib/errors.ts
@@ -1,3 +1,5 @@
+import { Prisma } from "@club/db/client";
+import { NextResponse } from "next/server";
 import { z } from "zod";
 
 export const ErrorCode = z.enum([
@@ -43,5 +45,50 @@ export class ClubApiError extends Error {
         if (Error.captureStackTrace) {
             Error.captureStackTrace(this, ClubApiError);
         }
+    }
+}
+
+export class PrismaErrorHandler {
+    private error: Prisma.PrismaClientKnownRequestError | unknown;
+
+    constructor(error: unknown) {
+        this.error = error
+    }
+
+    public handleError(): NextResponse<{
+        message: string,
+        code: number,
+    }> {
+        if (this.error instanceof Prisma.PrismaClientKnownRequestError) {
+            // get error code of prisma from => https://www.prisma.io/docs/orm/reference/error-reference#error-codes
+            switch (this.error.code) {
+                case 'P2002':
+                    return NextResponse.json({
+                        message: "A unique constraint violation occurred. Duplicate entry found.",
+                        code: 409
+                    }, { status: 409 });
+                case 'P2003':
+                    return NextResponse.json({
+                        message: "A foreign key constraint violation occurred.",
+                        code: 409
+
+                    }, { status: 400 });
+                case 'P2025':
+                    return NextResponse.json({
+                        message: "The record you are trying to modify does not exist.",
+                        code: 409
+
+                    }, { status: 404 });
+                default:
+                    return NextResponse.json({
+                        message: "A database error occurred.",
+                        code: 409
+                    }, { status: 500 });
+            }
+        }
+        return NextResponse.json({
+            message: "An unexpected error occurred.",
+            code: 500
+        }, { status: 500 });
     }
 }
