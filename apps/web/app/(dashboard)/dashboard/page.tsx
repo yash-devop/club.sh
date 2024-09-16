ea8cd11665c36e36357df87e2298f311dd02a86f
yash-devop <kamble1234meena@gmail.com>
2024-09-16 15:05:16 +0530

    chore: add dropdown to Avatar

--- apps/web/app/(dashboard)/dashboard/page.tsx
+++ apps/web/app/(dashboard)/dashboard/page.tsx
@@ -1,15 +1,48 @@
-import DashboardHeader from "@/app/components/DashboardHeader"
+import DashboardHeader from "@/app/components/dashboard/DashboardHeader"
+import LinksContainer from "@/app/components/dashboard/LinksContainer"
+import {
+    Avatar,
+    AvatarFallback,
+    AvatarImage
+} from '@club/ui'
 
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger
+} from '@club/ui'
+
+import {LogOut} from 'lucide-react'
 export default function DashboardPage() {
     return (
         <>
             <div className="w-full min-h-screen h-screen ">
                 <div>
                     <MaxWidthContainer className="flex items-center justify-between gap-3 p-5 ">
-                        <span>WEB-LOGO</span>
-                        <div>
-                            <span>USER-LOGO</span>
-                        </div>
+                        <p className="text-xl md:text-2xl font-semibold tracking-[-2px]">club.url </p>
+
+                        <DropdownMenu>
+                            <DropdownMenuTrigger>
+                                <Avatar>
+                                    <AvatarImage src="https://github.com/shadcn.png" />
+                                    <AvatarFallback className="bg-slate-300">CN</AvatarFallback>
+                                </Avatar>
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent align="end" className="px-2 py-3 min-w-0">
+                                <DropdownMenuLabel className="min-w-0">
+                                    <span>My Account</span>
+                                    <p className="truncate text-slate-500 font-light text-sm cursor-pointer hover:text-black">kamble1234meena@gmail.com</p>
+                                </DropdownMenuLabel>
+                                <DropdownMenuSeparator />
+                                <DropdownMenuItem className="gap-2">
+                                    <LogOut size={15}/>
+                                    <p>Log out</p>
+                                </DropdownMenuItem>
+                            </DropdownMenuContent>
+                        </DropdownMenu>
                     </MaxWidthContainer>
                 </div>
                 <BorderContainer>
@@ -18,10 +51,14 @@ export default function DashboardPage() {
                     </MaxWidthContainer>
                 </BorderContainer>
                 <div className="h-full w-full bg-gray-50">
-                    <MaxWidthContainer className="px-8 py-14">
-                        <div className="flex items-center justify-between">
-                            <p className="font-medium text-2xl tracking-tight">Links</p>
+                    <MaxWidthContainer className="px-4 md:px-8 py-14">
+                        <div className="flex items-center justify-between pb-5">
+                            <p className="font-medium text-2xl tracking-tight pl-3 md:p-0">Links</p>
+                            <div className="bg-red-400 px-5 py-2 rounded-lg">
+                                <p>Create Link C</p>
+                            </div>
                         </div>
+                        <LinksContainer />
                     </MaxWidthContainer>
                 </div>
             </div>
