d6936d5825ef21b8e78b4ac88057687d10c86291
yash-devop <kamble1234meena@gmail.com>
2024-09-16 15:04:48 +0530

    add: dropdown to card

--- apps/web/app/components/dashboard/LinkCard.tsx
+++ apps/web/app/components/dashboard/LinkCard.tsx
@@ -1,5 +1,12 @@
-import { Copy, CornerDownRight } from "lucide-react";
-
+import { Copy, CornerDownRight, EllipsisVertical } from "lucide-react";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger
+} from '@club/ui'
 export default function LinkCard() {
     return (
         <>
@@ -12,22 +19,38 @@ export default function LinkCard() {
                             <path d="M16.0131 19.9999L26.425 14.0032L16.0131 8.00655V19.9999Z" fill="white" />
                         </svg>
                     </div>
-                    <div className="flex flex-col gap min-w-0">
-                        <div className="flex items-center gap-3">
-                            <span className="font-medium text-sm">club.sh/hiteshbhau</span>
-                            <Copy size={15} className="text-slate-400"/>
+                    <div className="flex items-center justify-between gap min-w-0 w-full">
+                        <div className="flex flex-col min-w-0">
+                            <div className="flex items-center gap-3">
+                                <span className="font-medium text-sm">club.sh/hiteshbhau</span>
+                                <Copy size={15} className="text-slate-400" />
+                            </div>
+                            <div className="flex items-center gap-3 min-w-0">
+                                <div className="flex items-center gap-1 min-w-0">
+                                    <CornerDownRight size={15} className="text-slate-500 cursor-pointer" />
+                                    <p className="truncate text-slate-500 font-light text-sm cursor-pointer hover:text-black">
+                                        https://www.youtube.com/watch?v=jt5bqO_r2DQ
+                                    </p>
+                                </div>
+                                <div className="flex items-center gap-3 text-slate-500">
+                                    <div className="w-4 h-4 rounded-full bg-slate-300 shrink-0 hidden md:block"></div>
+                                    <span className="text-xs hidden lg:block">Sep 2</span>
+                                </div>
+                            </div>
                         </div>
-                        <div className="flex items-center gap-3">
-                            <div className="flex items-center gap-1 min-w-0">
-                                <CornerDownRight size={15} className="text-slate-500 cursor-pointer"/>
-                                <p className="truncate text-slate-500 font-light text-sm cursor-pointer hover:text-black">
-                                    https://www.youtube.com/watch?v=jt5bqO_r2DQ
-                                </p>
-                            </div>
-                            <div className="flex items-center gap-3 text-slate-500">
-                                <div className="w-4 h-4 rounded-full bg-slate-300 shrink-0 "></div>
-                                <span className="text-xs">Sep 2</span>
-                            </div>
+                        <div className="flex items-center">
+                            <DropdownMenu>
+                                <DropdownMenuTrigger className="flex items-center">
+                                    <div className="border hover:bg-slate-50 rounded-md p-1.5 w-fit shrink-0 cursor-pointer">
+                                        <EllipsisVertical size={20} />
+                                    </div>
+                                </DropdownMenuTrigger>
+                                <DropdownMenuContent align="end" className="px-2 py-3 min-w-0">
+                                    <DropdownMenuItem>
+                                        <p>Edit Link</p>
+                                    </DropdownMenuItem>
+                                </DropdownMenuContent>
+                            </DropdownMenu>
                         </div>
                     </div>
                 </div>
