8359f250e8eb408099ff5f82ff84d6d42784c5b1
yash-devop <kamble1234meena@gmail.com>
2024-09-17 15:28:15 +0530

    add: Custom LinkLabel component made from Shadcn Label

---
+++ apps/web/app/components/dashboard/LinkLabel.tsx
@@ -0,0 +1,64 @@
+import { Label, Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@club/ui";
+import { HelpCircle } from "lucide-react";
+import React from "react";
+
+export default function LinkLabel({
+    label,
+    logo,
+    additional,
+    additionalContent,
+    labelDescription
+}: {
+    label: string,
+    labelDescription: string
+    logo: JSX.Element,
+    additional?: string | JSX.Element,
+    additionalContent?: string,
+}) {
+    return (
+        <>
+            <TooltipProvider delayDuration={100} >
+
+                <div className="flex items-center justify-between w-full ">
+                    <div className="space-x-2 flex items-center">
+                        <Label className="text-slate-700">{label}</Label>
+                        <Tooltip>
+                            <TooltipTrigger className="text-gray-400">
+                                {logo}
+                            </TooltipTrigger>
+                            <TooltipContent align="center" className="z-[1000]">
+                                <p className="text-wrap max-w-sm text-center">{labelDescription}</p>
+                            </TooltipContent>
+                        </Tooltip>
+                    </div>
+                    {
+                        typeof additional === "string" ? <p>{additional}</p> : null
+                    }
+                    {
+                        (React.isValidElement(additional) && typeof additional.type) ? (
+                            <>
+                                {/* <TooltipProvider> */}
+                                <div>
+                                    <Tooltip>
+                                        <TooltipTrigger className="text-slate-700 flex items-center">
+                                            {additional}
+                                        </TooltipTrigger>
+                                        {
+                                            additionalContent && (
+                                                <TooltipContent>
+                                                    {additionalContent}
+                                                </TooltipContent>
+                                            )
+                                        }
+                                    </Tooltip>
+                                </div>
+                                {/* </TooltipProvider> */}
+                            </>
+                        ) : null
+                    }
+                </div>
+            </TooltipProvider>
+
+        </>
+    )
+}
