25b3612393598e728abff172b4831fc7d513cd49
yash-devop <kamble1234meena@gmail.com>
2024-09-17 18:16:34 +0530

    chore: moved Tooltip provider here from linkLabel

--- apps/web/app/providers.tsx
+++ apps/web/app/providers.tsx
@@ -1,17 +1,20 @@
 "use client"
 import { SessionProvider } from "next-auth/react";
 import { ModalProvider } from "./components/modals/ModalProvider";
+import { TooltipProvider } from "@club/ui";
 
 export default function Providers({ children }: {
     children: React.ReactNode
 }) {
     return (
         <>
-            <SessionProvider>
-                <ModalProvider>
-                    {children}
-                </ModalProvider>
-            </SessionProvider>
+            <TooltipProvider delayDuration={100}>
+                <SessionProvider>
+                    <ModalProvider>
+                        {children}
+                    </ModalProvider>
+                </SessionProvider>
+            </TooltipProvider>
         </>
     )
 }
