7b3cc8437ca29bd18c553e501c9b7896b2c4bd5c
yash-devop <kamble1234meena@gmail.com>
2024-09-17 09:31:42 +0530

    chore: change div to button

--- apps/web/app/components/dashboard/LinksHeader.tsx
+++ apps/web/app/components/dashboard/LinksHeader.tsx
@@ -1,17 +1,18 @@
 "use client"
 
+import { Button } from "@club/ui"
 import { useCreateLinkModal } from "../modals/create-link-modal"
 
 export default function LinksHeader() {
     const {CreateModalCallback , setShowCreateLinkModal} = useCreateLinkModal()
-    
+
     return (
         <>
             <div className="flex items-center justify-between pb-5">
                 <p className="font-medium text-2xl tracking-tight pl-3 md:p-0">Links</p>
-                <div className="bg-red-400 px-5 py-2 rounded-lg" onClick={() => setShowCreateLinkModal(true)}>
+                <Button className="transition-all hover:outline outline-slate-300 outline-offset-[0.5px]" onClick={() => setShowCreateLinkModal(true)}>
                     <p>Create Link C</p>
-                </div>
+                </Button>
                 <CreateModalCallback />
             </div>
         </>
