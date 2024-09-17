ce24f48fdcaa12f96d9d9b54accb982f9688dfcc
yash-devop <kamble1234meena@gmail.com>
2024-09-17 09:12:53 +0530

    chore: refactor the useContext logic

--- apps/web/app/components/modals/ModalProvider.tsx
+++ apps/web/app/components/modals/ModalProvider.tsx
@@ -1,26 +1,23 @@
-"use client"
+"use client";
 
-import { createContext, Dispatch, SetStateAction, useState } from "react"
-import {useCreateLinkModal} from "./create-link-modal"
+import { createContext, Dispatch, SetStateAction, ReactNode, useContext } from "react";
+import { useCreateLinkModal } from "./create-link-modal";
 
-export const ModalContext = createContext<{
-    setShowCreateLinkModal: Dispatch<React.SetStateAction<boolean>>
-}>({
-    setShowCreateLinkModal: () => {}
-})
-
-export default function ModalProvider({
-    children
-}: {
-    children: React.ReactNode
-}) {
-    const {CreateModalCallback , setShowCreateLinkModal} = useCreateLinkModal()
-    return (
-        <>
-            <ModalContext.Provider value={{setShowCreateLinkModal}}>
-                <CreateModalCallback />
-                {children}
-            </ModalContext.Provider>
-        </>
-    )
+type ModalContextProps = {
+    showCreateLinkModal: boolean,
+    setShowCreateLinkModal: React.Dispatch<React.SetStateAction<boolean>>
 }
+
+export const ModalContext = createContext<ModalContextProps>({
+    showCreateLinkModal: false,
+    setShowCreateLinkModal: () => { }
+})
+export const ModalProvider = ({ children }: { children: ReactNode }) => {
+    const { showCreateLinkModal, setShowCreateLinkModal, CreateModalCallback } = useCreateLinkModal()
+    return (
+        <ModalContext.Provider value={{ showCreateLinkModal, setShowCreateLinkModal }}>
+            <CreateModalCallback />
+            {children}
+        </ModalContext.Provider>
+    );
+};
