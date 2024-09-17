07d99123358645013e795ba484e93ae11d14b8dc
yash-devop <kamble1234meena@gmail.com>
2024-09-17 18:17:17 +0530

    add: link label and moved tooltip provider to providers.tsx

--- apps/web/app/components/dashboard/LinkLabel.tsx
+++ apps/web/app/components/dashboard/LinkLabel.tsx
@@ -17,47 +17,43 @@ export default function LinkLabel({
 }) {
     return (
         <>
-            <TooltipProvider delayDuration={100} >
 
-                <div className="flex items-center justify-between w-full ">
-                    <div className="space-x-2 flex items-center">
-                        <Label className="text-slate-700">{label}</Label>
-                        <Tooltip>
-                            <TooltipTrigger className="text-gray-400">
-                                {logo}
-                            </TooltipTrigger>
-                            <TooltipContent align="center" className="z-[1000]">
-                                <p className="text-wrap max-w-sm text-center">{labelDescription}</p>
-                            </TooltipContent>
-                        </Tooltip>
-                    </div>
-                    {
-                        typeof additional === "string" ? <p>{additional}</p> : null
-                    }
-                    {
-                        (React.isValidElement(additional) && typeof additional.type) ? (
-                            <>
-                                {/* <TooltipProvider> */}
-                                <div>
-                                    <Tooltip>
-                                        <TooltipTrigger className="text-slate-700 flex items-center">
-                                            {additional}
-                                        </TooltipTrigger>
-                                        {
-                                            additionalContent && (
-                                                <TooltipContent>
-                                                    {additionalContent}
-                                                </TooltipContent>
-                                            )
-                                        }
-                                    </Tooltip>
-                                </div>
-                                {/* </TooltipProvider> */}
-                            </>
-                        ) : null
-                    }
+            <div className="flex items-center justify-between w-full ">
+                <div className="space-x-2 flex items-center">
+                    <Label className="text-slate-700">{label}</Label>
+                    <Tooltip>
+                        <TooltipTrigger className="text-gray-400">
+                            {logo}
+                        </TooltipTrigger>
+                        <TooltipContent align="center" className="z-[1000]">
+                            <p className="text-wrap max-w-sm text-center">{labelDescription}</p>
+                        </TooltipContent>
+                    </Tooltip>
                 </div>
-            </TooltipProvider>
+                {
+                    typeof additional === "string" ? <p>{additional}</p> : null
+                }
+                {
+                    (React.isValidElement(additional) && typeof additional.type) ? (
+                        <>
+                            <div>
+                                <Tooltip>
+                                    <TooltipTrigger className="text-slate-700 flex items-center">
+                                        {additional}
+                                    </TooltipTrigger>
+                                    {
+                                        additionalContent && (
+                                            <TooltipContent>
+                                                {additionalContent}
+                                            </TooltipContent>
+                                        )
+                                    }
+                                </Tooltip>
+                            </div>
+                        </>
+                    ) : null
+                }
+            </div>
 
         </>
     )
