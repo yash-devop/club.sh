34e783a1bb91984a32733f3ed4632d23d358a31b
yash-devop <kamble1234meena@gmail.com>
2024-09-17 09:31:24 +0530

    chore: add the useCallback and useMemo for performance optimisation

--- apps/web/app/components/modals/create-link-modal.tsx
+++ apps/web/app/components/modals/create-link-modal.tsx
@@ -1,23 +1,25 @@
 "use client";
 
-import { useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 import { Modal } from "@club/ui";
 
 export function useCreateLinkModal() {
   const [showCreateLinkModal, setShowCreateLinkModal] = useState<boolean>(false);
 
-  const CreateModalCallback = () => (
+  const CreateModalCallback = useCallback(() => (       // return
     <Modal
       showModal={showCreateLinkModal}
       setShowModal={setShowCreateLinkModal}
     >
       <p>I am a Modal from Context API</p>
     </Modal>
-  );
+  ),[showCreateLinkModal , setShowCreateLinkModal])
 
-  return {
-    showCreateLinkModal,
-    CreateModalCallback,
-    setShowCreateLinkModal,
-  };
-}
+  return useMemo(()=>{
+     return {
+         showCreateLinkModal,
+         CreateModalCallback,
+         setShowCreateLinkModal,
+     }
+  },[showCreateLinkModal,CreateModalCallback, setShowCreateLinkModal])
+} 
