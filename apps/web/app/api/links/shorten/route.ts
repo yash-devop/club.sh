896b197379f37609b24adc88116bed19d2bc4c6e
yash-devop <kamble1234meena@gmail.com>
2024-09-18 18:56:47 +0530

    chore: handle minor edge cases

--- apps/web/app/api/links/shorten/route.ts
+++ apps/web/app/api/links/shorten/route.ts
@@ -1,9 +1,8 @@
 import { NextRequest, NextResponse } from "next/server"
-import { hashString } from '@club/utils'
+import { getShortCode } from '@club/utils'
 
-import { CreateLinkProps, LinkProps } from '@/lib/types'
+import { CreateLinkProps } from '@/lib/types'
 import prisma from "@club/db/client";
-import {Prisma} from "@club/db/client";
 import { ClubApiError, PrismaErrorHandler } from "@/lib/errors";
 import { getSessionFn } from "@/lib/auth/getSession";
 export const POST = async (req: NextRequest, res: NextResponse) => {
@@ -11,61 +10,119 @@ export const POST = async (req: NextRequest, res: NextResponse) => {
     if (!session?.user) {
         return new Response("Unauthorized Access. Please login", { status: 401 });
     }
+    const { url ,shortCode }: CreateLinkProps = await req.json();
+    console.log({
+        url,
+        shortCode
+    });
     try {
-
-
-        const { url }: CreateLinkProps = await req.json();
-        const hashedStr = hashString(url)
-        const base64Hash = btoa(hashedStr);     // AjYyY2M0NzAzMGIxODAzMDY0ODQ0Yjk0YzFjYjAwNTRhNRe3ZDFlNTUwZTI2YmIzM2YyMTUxNDlkOGIyYzcyZQ==
-
-        const cleanedString = base64Hash.replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '');            //regex to cleanup + , / , ==
-
-        const truncatedStr = cleanedString.substring(0, 7);
-
-
-        const existingShortCode = await prisma.link.findUnique({
-            where: {
-                shortCode: truncatedStr,
-                user: {
-                    id: session.user.id
+        if (!url) {
+            throw new ClubApiError({
+                code: 404,
+                errorName: "not_found",
+                message: "URL not found !!",
+                inputField: {
+                    "shortCode": "The Short link Already Exists: Duplicate Key Found !"
                 }
+            })
+        }
+        if(!shortCode){
+            const truncatedStr = getShortCode(url)
+            const existingShortCode = await prisma.link.findUnique({
+                where: {
+                    shortCode: truncatedStr,            // useful when user has the option of `Add custom id`
+                    user: {
+                        id: session.user.id
+                    }
+                }
+            })
+            console.log('existing : ', existingShortCode);
+            if (existingShortCode) {
+                throw new ClubApiError({
+                    errorName: "conflict",
+                    code: 409,
+                    message: "The Short link Already Exists: Duplicate Key Found !",
+                    inputField: {
+                        "shortCode": "The Short link Already Exists: Duplicate Key Found !"
+                    }
+                })
             }
-        })
-        if (existingShortCode) {
-            throw new ClubApiError({
-                errorName: "conflict",
-                code: 409,
-                message: "The URL Already Exists: Duplicate Key Found !"
+    
+            const newLink = await prisma.link.create({
+                data: {
+                    url,
+                    shortCode: truncatedStr,
+                    userId: session.user.id
+                },
+            })
+    
+            if (!newLink) {
+                throw new ClubApiError({
+                    errorName: "conflict",
+                    code: 409,
+                    message: "The Short link Already Exists: Duplicate Key Found !",
+                    inputField: {
+                        "shortCode": "The Short link Already Exists: Duplicate Key Found !"
+                    }
+                })
+            }
+            return NextResponse.json({
+                message: "Shorten Link Created",
+                link: newLink
+            })
+        }
+        else {
+            const existingShortCode = await prisma.link.findUnique({
+                where: {
+                    shortCode,            // useful when user has the option of `Add custom id`
+                    user: {
+                        id: session.user.id
+                    }
+                }
+            })
+            console.log('existing : ', existingShortCode);
+            if (existingShortCode) {
+                throw new ClubApiError({
+                    errorName: "conflict",
+                    code: 409,
+                    message: "The Short link Already Exists: Duplicate Key Found !",
+                    inputField: {
+                        "shortCode": "The Short link Already Exists: Duplicate Key Found !"
+                    }
+                })
+            }
+    
+            const newLink = await prisma.link.create({
+                data: {
+                    url,
+                    shortCode,
+                    userId: session.user.id,
+                },
+            })
+    
+            if (!newLink) {
+                throw new ClubApiError({
+                    errorName: "conflict",
+                    code: 409,
+                    message: "ERROR Hai re baba conflict vali",
+                    inputField: {
+                        "shortCode": "Conflict"
+                    }
+                })
+            }
+            return NextResponse.json({
+                message: "Shorten Link Created",
+                link: newLink
             })
         }
 
-        const newLink = await prisma.link.create({
-            data: {
-                url,
-                shortCode: truncatedStr,
-                userId: session.user.id
-            },
-        })
 
-        if(!newLink){
-            throw new ClubApiError({
-                errorName:"conflict",
-                code:409,
-                message: "ERROR Hai re baba conflict vali"
-            })
-        }
-
-        return NextResponse.json({
-            message: "Link Created",
-            link: newLink
-        }, {
-
-        })
+        
     } catch (error) {
         if (error instanceof ClubApiError) {
-            return NextResponse.json({ message: error.message }, { status: error.code });
+            return NextResponse.json({ message: error.message , field: error.inputField }, { status: error.code });
         }
-        
+
         const prismaErrorHandler = new PrismaErrorHandler(error)
         return prismaErrorHandler.handleError()
     }
