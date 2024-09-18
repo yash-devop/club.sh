e28204c4e885d41fd1fb8688847b5f451a1f1b0a
yash-devop <kamble1234meena@gmail.com>
2024-09-18 18:56:12 +0530

    add: QueryClient and DevTools from Tanstack react-query

--- apps/web/app/providers.tsx
+++ apps/web/app/providers.tsx
@@ -2,17 +2,29 @@
 import { SessionProvider } from "next-auth/react";
 import { ModalProvider } from "./components/modals/ModalProvider";
 import { TooltipProvider } from "@club/ui";
+import { QueryClient  , QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export default function Providers({ children }: {
     children: React.ReactNode
 }) {
+    const queryClient = new QueryClient({
+        defaultOptions:{
+            queries:{
+                refetchOnWindowFocus: false
+            }
+        }
+    })
     return (
         <>
             <TooltipProvider delayDuration={100}>
                 <SessionProvider>
-                    <ModalProvider>
-                        {children}
-                    </ModalProvider>
+                    <QueryClientProvider client={queryClient}>
+                        <ReactQueryDevtools />
+                        <ModalProvider>
+                            {children}
+                        </ModalProvider>
+                    </QueryClientProvider>
                 </SessionProvider>
             </TooltipProvider>
         </>
