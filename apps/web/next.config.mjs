99831cd50659e711f1cc4e1d0a853cb862ec6adf
yash-devop <kamble1234meena@gmail.com>
2024-09-21 21:10:39 +0530

    acceptable image paths

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,4 +1,31 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    // fixes the error null error of useContext ( look at : https://github.com/vercel/next.js/issues/63123#issuecomment-2002398438)
+    webpack: (config) => {
+      return {
+        ...config,
+        externals: [...config.externals, "pino-pretty", "encoding"],
+      };
+    },
+    images:{
+      remotePatterns:[
+        {
+          hostname: "s2.googleusercontent.com"
+        },
+        {
+          hostname: "www.google.com"
+        },
+        {
+          hostname:"picsum.photos"    // im using this for testing images ( for UI only)
+        },
+        {
+          hostname: "pbs.twimg.com"
+        },
+        {
+          hostname: "media.licdn.com"
+        }
+      ]
+    }
+  };
+  
+  export default nextConfig;
