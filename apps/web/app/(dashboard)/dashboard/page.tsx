4427fe5b632961fae1d53b1d0e238399cad023eb
yash-devop <kamble1234meena@gmail.com>
2024-09-23 11:28:07 +0530

    fix: user image not showing by adding next/Image

--- apps/web/app/(dashboard)/dashboard/page.tsx
+++ apps/web/app/(dashboard)/dashboard/page.tsx
@@ -1,6 +1,7 @@
 import DashboardHeader from "@/app/components/dashboard/DashboardHeader"
 import LinksContainer from "@/app/components/dashboard/LinksContainer"
 import LinksHeader from "@/app/components/dashboard/LinksHeader"
+import { Logout } from "@/app/components/Logout"
 import {
     Avatar,
     AvatarFallback,
@@ -17,36 +18,48 @@ import {
     DropdownMenuTrigger
 } from '@club/ui'
 
-import {LogOut} from 'lucide-react'
+import { LogOut } from 'lucide-react'
 import { getServerSession } from "next-auth"
+import Image from "next/image"
+import { redirect } from "next/navigation"
 export default async function DashboardPage() {
 
     const session = await getServerSession()
+
+    if (!session || !session.user) {
+        return redirect("/signin")
+    }
+
+    console.log('session?.user?.image ', session.user.image);
     return (
         <>
             <div className="w-full min-h-screen bg-gray-50 ">
                 <div className="bg-white">
                     <MaxWidthContainer className="flex items-center justify-between gap-3 p-5 ">
-                        <p className="text-xl md:text-2xl font-semibold tracking-[-2px]">club.url </p>
+                        <p className="text-xl md:text-2xl font-semibold tracking-[-2px] cursor-pointer">club.url </p>
 
                         <DropdownMenu>
                             <DropdownMenuTrigger>
                                 <Avatar>
-                                    {/* https://github.com/shadcn.png */}
-                                    <AvatarImage src={session?.user?.image!} />
-                                    <AvatarFallback className="bg-slate-300">CN</AvatarFallback>
+                                       <AvatarImage src={session.user.image || "https://github.com/shadcn.png"} asChild>
+                                        <Image
+                                            src={session.user.image || "https://github.com/shadcn.png"}
+                                            alt="User Profile Image"
+                                            width={40}  
+                                            height={40} 
+                                            className="rounded-full"
+                                        />
+                                    </AvatarImage>
+                                    <AvatarFallback className="bg-slate-300">CS</AvatarFallback>
                                 </Avatar>
                             </DropdownMenuTrigger>
                             <DropdownMenuContent align="end" className="px-2 py-3 min-w-0">
                                 <DropdownMenuLabel className="min-w-0">
-                                    <span>My Account</span>
-                                    <p className="truncate text-slate-500 font-light text-sm cursor-pointer hover:text-black">{session?.user?.email }</p>
+                                    <span>{session?.user?.name}</span>
+                                    <p className="truncate text-slate-500 font-light text-sm cursor-pointer hover:text-black">{session?.user?.email}</p>
                                 </DropdownMenuLabel>
                                 <DropdownMenuSeparator />
-                                <DropdownMenuItem className="gap-2">
-                                    <LogOut size={15}/>
-                                    <p>Log out</p>
-                                </DropdownMenuItem>
+                                <Logout />
                             </DropdownMenuContent>
                         </DropdownMenu>
                     </MaxWidthContainer>
@@ -56,10 +69,10 @@ export default async function DashboardPage() {
                         <DashboardHeader />
                     </MaxWidthContainer>
                 </BorderContainer>
-                    <MaxWidthContainer className="px-4 md:px-8 py-14">
-                        <LinksHeader />
-                        <LinksContainer />
-                    </MaxWidthContainer>
+                <MaxWidthContainer className="px-4 md:px-8 py-14">
+                    <LinksHeader />
+                    <LinksContainer />
+                </MaxWidthContainer>
             </div>
         </>
     )
@@ -75,7 +88,7 @@ const BorderContainer = ({
 }) => {
     return (
         <>
-            <div className={cn(`border-b bg-white`,className)}>
+            <div className={cn(`border-b bg-white`, className)}>
                 {children}
             </div>
         </>
