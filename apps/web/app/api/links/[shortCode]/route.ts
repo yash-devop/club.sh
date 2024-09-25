436e346ac59fde03141d51c39cc56c56c85c377d
yash-devop <kamble1234meena@gmail.com>
2024-09-25 09:13:53 +0530

    create ShortUrl

--- apps/web/app/api/links/[shortCode]/route.ts
+++ apps/web/app/api/links/[shortCode]/route.ts
@@ -17,17 +17,30 @@ export const GET = async (req: NextRequest, {
         const existingShortCode = await prisma.link.findUnique({
             where: {
                 shortCode
+            },
+            select:{
+                url: true,
+                title: true,
+                description: true,
+                image:true
             }
         })
         if (!existingShortCode) {
+            console.log('Shortcode nhi mila bhai ;-; ');
             throw new ClubApiError({
                 errorName: "not_found",
-                code: 200,
+                code: 404,
                 message: "The shorturl not found in database. Please check your URL correctly."
             })
         }
-        const og_url = existingShortCode.url
-        return NextResponse.redirect(og_url)
+        const {url , title ,description , image} = existingShortCode
+        return NextResponse.json({
+            url,
+            title,
+            description,
+            image
+        })
+        // return NextResponse.redirect(og_url)
     } catch (error) {
         if (error instanceof ClubApiError) {
             return NextResponse.json({ message: error.message }, { status: error.code });
