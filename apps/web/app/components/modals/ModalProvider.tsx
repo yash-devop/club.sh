50754c352620f4a78c4d35b70a80c8f360bd47ca
yash-devop <kamble1234meena@gmail.com>
2024-09-27 19:58:00 +0530

    add: QRCallback component

--- apps/web/app/components/modals/ModalProvider.tsx
+++ apps/web/app/components/modals/ModalProvider.tsx
@@ -2,21 +2,28 @@
 
 import { createContext, ReactNode } from "react";
 import { useCreateLinkModal } from "./create-link-modal";
+import { useQrModal } from "./qr-modal";
 
 type ModalContextProps = {
     showCreateLinkModal: boolean,
     setShowCreateLinkModal: React.Dispatch<React.SetStateAction<boolean>>
+    showQRModal: boolean,
+    setShowQRModal: React.Dispatch<React.SetStateAction<boolean>>
 }
 
 export const ModalContext = createContext<ModalContextProps>({
     showCreateLinkModal: false,
-    setShowCreateLinkModal: () => { }
+    setShowCreateLinkModal: () => { },
+    showQRModal: false,
+    setShowQRModal: () => { }
 })
 export const ModalProvider = ({ children }: { children: ReactNode }) => {
     const { showCreateLinkModal, setShowCreateLinkModal, CreateModalCallback } = useCreateLinkModal()
+    const { showQRModal, QRCallback, setShowQRModal } = useQrModal();
     return (
-        <ModalContext.Provider value={{ showCreateLinkModal, setShowCreateLinkModal }}>
+        <ModalContext.Provider value={{ showCreateLinkModal, setShowCreateLinkModal, showQRModal, setShowQRModal }}>
             <CreateModalCallback />
+            <QRCallback />
             {children}
         </ModalContext.Provider>
     );
