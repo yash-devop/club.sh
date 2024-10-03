da4b03c436d4e10465811e7ef1affca4e43e1870
yash-devop <kamble1234meena@gmail.com>
2024-10-03 15:00:34 +0530

    add: development geodata

---
+++ packages/utils/constants/localhost.ts
@@ -0,0 +1,9 @@
+export const LOCALHOST_GEO_DATA = {
+    // continent: "NA",
+    country: "IN",
+    city: "Panvel",
+    region: "Maharastra",
+    // latitude: "19.03227",
+    // longitude: "73.04276",
+  };
+  export const LOCALHOST_IP = "103.111.231.188";
