2704aa6fa6e170464852a0c5b13ae59c93112bb1
yash-devop <kamble1234meena@gmail.com>
2024-10-03 15:01:45 +0530

    add: delete modal

--- apps/web/app/components/modals/ModalProvider.tsx
+++ apps/web/app/components/modals/ModalProvider.tsx
@@ -3,27 +3,34 @@
 import { createContext, ReactNode } from "react";
 import { useCreateLinkModal } from "./create-link-modal";
 import { useQrModal } from "./qr-modal";
+import { useDeleteModal } from "./delete-link-modal";
 
 type ModalContextProps = {
     showCreateLinkModal: boolean,
     setShowCreateLinkModal: React.Dispatch<React.SetStateAction<boolean>>
     showQRModal: boolean,
     setShowQRModal: React.Dispatch<React.SetStateAction<boolean>>
+    showDeleteModal: boolean;
+    setShowDeleteModal: React.Dispatch<React.SetStateAction<boolean>>
 }
 
 export const ModalContext = createContext<ModalContextProps>({
     showCreateLinkModal: false,
-    setShowCreateLinkModal: () => { },
+    setShowCreateLinkModal: () => {},
     showQRModal: false,
-    setShowQRModal: () => { }
+    setShowQRModal: () => {},
+    showDeleteModal: false,
+    setShowDeleteModal: () => {}
 })
 export const ModalProvider = ({ children }: { children: ReactNode }) => {
     const { showCreateLinkModal, setShowCreateLinkModal, CreateModalCallback } = useCreateLinkModal()
     const { showQRModal, QRCallback, setShowQRModal } = useQrModal();
+    const { showDeleteModal , DeleteCallback , setShowDeleteModal} = useDeleteModal();
     return (
-        <ModalContext.Provider value={{ showCreateLinkModal, setShowCreateLinkModal, showQRModal, setShowQRModal }}>
+        <ModalContext.Provider value={{ showCreateLinkModal, setShowCreateLinkModal, showQRModal, setShowQRModal , showDeleteModal, setShowDeleteModal}}>
             <CreateModalCallback />
             <QRCallback />
+            <DeleteCallback />
             {children}
         </ModalContext.Provider>
     );
