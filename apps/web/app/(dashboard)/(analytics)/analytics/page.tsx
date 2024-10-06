19a0bc6322922079d34720eb90b50f88debc277f
yash-devop <kamble1234meena@gmail.com>
2024-10-06 15:01:34 +0530

    add: Analytics Page with the defined components

--- apps/web/app/(dashboard)/(analytics)/analytics/page.tsx
+++ apps/web/app/(dashboard)/(analytics)/analytics/page.tsx
@@ -1,9 +1,15 @@
 import React from "react";
+import Country from "@/app/components/analytics/Country";
+import Devices from "@/app/components/analytics/Devices";
+import Referrer from "@/app/components/analytics/Referrer";
 
-export default function AnalyticsPage(){
+export default function AnalyticsPage() {
     return (
-        <>
-            <p>Analytics page here...</p>
-        </>
-    )
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 h-full">
+            {/* <Links /> */}
+            <Country />
+            <Devices />
+            <Referrer />
+        </div>
+    );
 }
