1569618ef2b6b63d85c6aaecfc6b64bafd14377b
yash-devop <kamble1234meena@gmail.com>
2024-10-07 15:02:02 +0530

    add: Links Component

--- apps/web/app/(dashboard)/(analytics)/analytics/page.tsx
+++ apps/web/app/(dashboard)/(analytics)/analytics/page.tsx
@@ -2,11 +2,12 @@ import React from "react";
 import Country from "@/app/components/analytics/Country";
 import Devices from "@/app/components/analytics/Devices";
 import Referrer from "@/app/components/analytics/Referrer";
+import Links from "@/app/components/analytics/Links";
 
 export default function AnalyticsPage() {
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6 h-full">
-            {/* <Links /> */}
+            <Links />
             <Country />
             <Devices />
             <Referrer />
