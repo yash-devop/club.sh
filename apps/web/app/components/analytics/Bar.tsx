bc2ad43697af18603894641ec7f142e2d2e8394f
yash-devop <kamble1234meena@gmail.com>
2024-10-07 15:00:17 +0530

    chore: change groupname from links to url

--- apps/web/app/components/analytics/Bar.tsx
+++ apps/web/app/components/analytics/Bar.tsx
@@ -16,15 +16,15 @@ export default function Bar({
         if(group === "device" || group === "browser" || group === "os") return 'bg-green-100';
         else if(group === "country") return 'bg-blue-100';
         else if(group === "referrer") return 'bg-fuchsia-100';
-        else if(group === "link") return 'bg-orange-100';
+        else if(group === "url") return 'bg-orange-100';
         else return 'bg-slate-100'
     }
     return (
         <>
             <div className=" flex items-center justify-between text-sm">
                 <div style={{
-                    width: `${dynamicWidth}%`
-                }} className={`${dynamicBackgrounds(group)} py-2 px-3 mr-4 rounded-lg font-normal transition-all`}>
+                    width: `${dynamicWidth}%`,
+                }} className={`${dynamicBackgrounds(group)} py-2 px-3 mr-4 rounded-lg font-normal transition-all whitespace-nowrap`}>
                     <p>{name}</p>
                 </div>
                 <p className="pr-4">{clicks}</p>
