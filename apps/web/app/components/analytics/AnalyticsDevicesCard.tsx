d243c69aeee2db29986f96433f4908dbe8311cc0
yash-devop <kamble1234meena@gmail.com>
2024-10-07 15:00:57 +0530

    chore: make clickIcon hidden on mobile

--- apps/web/app/components/analytics/AnalyticsDevicesCard.tsx
+++ apps/web/app/components/analytics/AnalyticsDevicesCard.tsx
@@ -103,7 +103,7 @@ export function AnalyticsDevicesCard<T extends string>({
         <div className="border bg-white min-h-[400px] max-h-[400px] w-full h-full rounded-xl overflow-auto relative">
             <div className="flex items-center justify-between border-b px-4 bg-white sticky top-0 right-0 left-0">
                 <Tabs options={tabs} selected={selectedTabId} onSelect={onSelectTab} />
-                <div className="flex items-center gap-1">
+                <div className="items-center gap-1 hidden sm:flex">
                     <MousePointerClick size={15} className="text-slate-600" />
                     <p className="font-light text-xs text-slate-400">CLICKS</p>
                 </div>
