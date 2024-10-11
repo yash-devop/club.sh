96ab5f676b58d3a7293d613248fa9bc531fd864b
yash-devop <kamble1234meena@gmail.com>
2024-10-11 09:45:48 +0530

    getGeoData

--- apps/web/lib/functions/getGeoData.ts
+++ apps/web/lib/functions/getGeoData.ts
@@ -2,7 +2,6 @@ export const getGeoData = async (ipAddress: string): Promise<{
     country: string;
     region: string;
 }> => {
-
     const IP_API_URL = process.env.IP_URL as string
     const IP_KEY = process.env.IP_KEY as string
     const response = await fetch(`${IP_API_URL}?apiKey=${IP_KEY}&ipAddress=${ipAddress}`, {
