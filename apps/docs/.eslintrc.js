8f0d1df02351c4d2b099cd4895250a3a8bb6913b
yash-devop <kamble1234meena@gmail.com>
2024-10-12 09:19:49 +0530

    rm: docs

--- apps/docs/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-/** @type {import("eslint").Linter.Config} */
-module.exports = {
-  root: true,
-  extends: ["@club/eslint-config/next.js"],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: true,
-  },
-};
