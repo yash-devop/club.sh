6e452b9e9ff62044d782db6e5e5acdacba0eec84
yash-devop <kamble1234meena@gmail.com>
2025-01-14 22:29:57 +0530

    add: next auth options

--- apps/web/lib/auth/options.ts
+++ apps/web/lib/auth/options.ts
@@ -3,47 +3,80 @@ import { NextAuthOptions, SessionStrategy } from "next-auth";
 import CredentialProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
 
-import prisma from '@club/db/client';
+import prisma from "@club/db/client";
 
-const GOOGLE_ID = process.env.GOOGLE_ID || ""
-const GOOGLE_SECRET = process.env.GOOGLE_SECRET || ""
+const GOOGLE_ID = process.env.GOOGLE_ID || "";
+const GOOGLE_SECRET = process.env.GOOGLE_SECRET || "";
 
 export const NEXT_AUTH_OPTIONS: NextAuthOptions = {
-    providers: [
-        GoogleProvider({
-            clientId: GOOGLE_ID,
-            clientSecret: GOOGLE_SECRET,
-            allowDangerousEmailAccountLinking: true,
-            authorization: `https://accounts.google.com/o/oauth2/auth/authorize?response_type=code&prompt=login`
-        })
-    ],
-    session: {
-        strategy: "jwt" as SessionStrategy
+  providers: [
+    GoogleProvider({
+      clientId: GOOGLE_ID,
+      clientSecret: GOOGLE_SECRET,
+      allowDangerousEmailAccountLinking: true,
+      authorization: `https://accounts.google.com/o/oauth2/auth/authorize?response_type=code&prompt=login`,
+    }),
+  ],
+  session: {
+    strategy: "jwt" as SessionStrategy,
+  },
+  pages: {
+    signIn: "/signin",
+    error: "/login",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  adapter: PrismaAdapter(prisma),
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        return {
+          ...token,
+          id: user.id,
+        };
+      }
+      return token;
     },
-    pages: {
-        signIn: "/signin",
-        error: "/login"
+    async signIn({ user, account }) {
+      // Check if the user exists, if not, create a new one
+      const existingUser = await prisma.user.findUnique({
+        where: { email: user.email as string },
+      });
+      if (!existingUser) {
+        await prisma.user.create({
+          data: {
+            email: user.email,
+            name: user.name,
+            image: user.image!,
+            // Add other fields as needed
+          },
+        });
+      }
+      return true;
+      // if (user && user.email) {
+      //   const existingUser = await prisma.user.findUnique({
+      //     where: { email: user.email as string },
+      //   });
+      //   if (!existingUser) {
+      //     await prisma.user.create({
+      //       data: {
+      //         email: user.email,
+      //         name: user.name,
+      //         image: user.image!,
+      //         // Add other fields as needed
+      //       },
+      //     });
+      //   }
+      // }
+      // return true;
     },
-    secret: process.env.NEXTAUTH_SECRET,
-    adapter: PrismaAdapter(prisma),
-    callbacks: {
-        async jwt({ token , user}) {
-            if(user){
-                return {
-                    ...token,
-                    id: user.id
-                }
-            }
-            return token
+    session: async ({ session, token, user }) => {
+      return {
+        ...session,
+        user: {
+          ...session.user,
+          id: token.sub,
         },
-        session: async ({ session, token, user }) => {
-           return {
-                ...session,
-                user: {
-                    ...session.user,
-                    id: token.sub
-                }
-           }
-         },
+      };
     },
+  },
 };
