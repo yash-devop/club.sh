8c4137518eba20cbcc1b76c577d23de1172f70c6
yash-devop <kamble1234meena@gmail.com>
2025-01-14 22:30:46 +0530

    chore: rename to club

--- apps/web/app/components/AuthLayout.tsx
+++ apps/web/app/components/AuthLayout.tsx
@@ -10,7 +10,7 @@ export const AuthLayout = ({
             <div className="h-screen w-full flex">
                 <div className="h-full w-full flex flex-col gap-4 items-center justify-center  mx-auto">
                     <div className="h-full w-full flex flex-col gap-4 items-center justify-center mx-auto">
-                        <p className="text-[35px] md:text-[40px] font-semibold tracking-[-2px] py-4">club.sh</p>
+                        <p className="text-[35px] md:text-[40px] font-semibold tracking-[-2px] py-4">club</p>
                         <div className="max-w-md w-full mx-auto flex flex-col justify-center h-fit md:rounded-2xl">
                             <div className="bg-white shadow border border-border p-7 text-center md:rounded-t-2xl">
                                 <h1 className="font-medium text-lg">{variant === "login" ? "Sign in to your Club Account" : "Create a Club account"}</h1>
@@ -22,7 +22,7 @@ export const AuthLayout = ({
                         {/* <div className="text-sm">{variant === "login" ? <p>Don't have an account ? <Link className="underline" href={"/signup"}>Sign up</Link></p> : <p>Already have an account ? <Link className="underline" href={"/signin"}>Sign in</Link></p>}</div> */}
                     
                     </div>
-                    <p className="pb-10 font-normal text-black/50 cursor-default select-none">© {new Date().getFullYear()} club.sh</p>
+                    <p className="pb-10 font-normal text-black/50 cursor-default select-none">© {new Date().getFullYear()} club</p>
                 </div>
                 <div className="h-full w-2/3 overflow-hidden relative border-l hidden lg:flex">
                 <div className="absolute inset-0 -z-10 h-full w-full bg-gray-50/90 bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]"></div>
--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -8,7 +8,7 @@ const inter_font = Inter({
   weight:["100","200","300","400","500","600","700","800"]
 })
 export const metadata: Metadata = {
-  title: "Club.sh",
+  title: "club",
   description: "NextJS URL Shortener",
 };
 
