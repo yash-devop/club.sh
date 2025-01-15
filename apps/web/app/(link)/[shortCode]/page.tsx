97c63a33f605ad2a413441b734b5859366c88f94
yash-devop <kamble1234meena@gmail.com>
2025-01-15 18:03:09 +0530

    rm: headers

--- apps/web/app/(link)/[shortCode]/page.tsx
+++ apps/web/app/(link)/[shortCode]/page.tsx
@@ -1,4 +1,3 @@
-import { headers } from "next/headers";             // i used headers from nextjs because to pass it to the ServerComponent... ( not possible to grab req in params just like we do in Route.ts )
 import { notFound, redirect } from "next/navigation";
 
 export default async function Page({
@@ -11,10 +10,7 @@ export default async function Page({
     },
 }) {
 
-
-    const response = await fetch(`http://localhost:3000/api/links/${shortCode}`, {
-        headers: headers()
-    });
+    const response = await fetch(`http://localhost:3000/api/links/${shortCode}`);
     const data = await response.json()
 
     if (response.status === 404 || typeof data.url === "undefined") {
