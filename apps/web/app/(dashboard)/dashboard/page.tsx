6c9138e3903e883e4ecb3a39cb543240605bd998
yash-devop <kamble1234meena@gmail.com>
2025-01-15 13:26:28 +0530

    chore: removed unused imports

--- apps/web/app/(dashboard)/dashboard/page.tsx
+++ apps/web/app/(dashboard)/dashboard/page.tsx
@@ -1,26 +1,6 @@
-import DashboardHeader from "@/app/components/dashboard/DashboardHeader"
 import LinksContainer from "@/app/components/dashboard/LinksContainer"
 import LinksHeader from "@/app/components/dashboard/LinksHeader"
-import { Logout } from "@/app/components/Logout"
-import {
-    Avatar,
-    AvatarFallback,
-    AvatarImage,
-    cn
-} from '@club/ui'
-
-import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuLabel,
-    DropdownMenuSeparator,
-    DropdownMenuTrigger
-} from '@club/ui'
-
-import { LogOut } from 'lucide-react'
 import { getServerSession } from "next-auth"
-import Image from "next/image"
 import { redirect } from "next/navigation"
 export default async function DashboardPage() {
     const session = await getServerSession()
