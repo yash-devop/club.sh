08194340d6e1082ec6bd49d21319e3ad0054c6e0
yash-devop <kamble1234meena@gmail.com>
2025-01-15 11:05:12 +0530

    add: user session

--- apps/web/app/components/Navbar.tsx
+++ apps/web/app/components/Navbar.tsx
@@ -1,19 +1,40 @@
-import { MaxWidthContainer } from "@club/ui";
+import { Button, MaxWidthContainer } from "@club/ui";
 import SignupButton from "./SignupButton";
+import Link from "next/link";
+import { getSessionFn } from "@/lib/auth/getSession";
+import { ArrowRight, ChevronRight } from "lucide-react";
+export default async function Navbar() {
+  const session = await getSessionFn();
 
-export default function Navbar() {
-    return (
-        <>
-            <div className="min-h-[65px] h-full border-b flex items-center bg-white px-4">
-                <MaxWidthContainer>
-                    <div className="h-full flex items-center justify-between">
-                        <p className="font-semibold text-2xl tracking-tighter">club.url</p>
-                        <a href="/signin" rel="">
-                            <SignupButton name="Get Started" type="primary" />
-                        </a>
-                    </div>
-                </MaxWidthContainer>
+  return (
+    <>
+      <div className="min-h-[65px] h-full border-b flex items-center bg-white px-4">
+        <MaxWidthContainer>
+          <div className="h-full flex items-center justify-between">
+            <p className="font-semibold text-2xl tracking-tighter">club.url</p>
+            <div className="flex items-center justify-end gap-3">
+              {session && session.user ? (
+                <>
+                  <Link href="/dashboard" rel="">
+                    <SignupButton
+                      name="Dashboard"
+                      type="primary"
+                      Icon={ChevronRight}
+                      className="pl-3 pr-2"
+                    />
+                  </Link>
+                </>
+              ) : (
+                <>
+                  <Link href="/signin" rel="">
+                    <SignupButton name="Get Started" type="primary" />
+                  </Link>
+                </>
+              )}
             </div>
-        </>
-    )
+          </div>
+        </MaxWidthContainer>
+      </div>
+    </>
+  );
 }
