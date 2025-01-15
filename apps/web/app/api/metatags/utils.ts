2704c7b0fc211cb803d4cf3bd2b68317f4c5069f
yash-devop <kamble1234meena@gmail.com>
2025-01-15 20:40:39 +0530

    added: he html decoder

--- apps/web/app/api/metatags/utils.ts
+++ apps/web/app/api/metatags/utils.ts
@@ -1,67 +1,74 @@
 import parse from "node-html-parser";
+import he from "he";
 
 export const getHtmlfromURL = async (url: string) => {
-    try {
-        const html = await fetch(url, {
-            headers: {
-                "User-Agent": "Club Metatag API (https://club.yashstack.com)"
-            }
-        });
-        const text = await html.text()
-        return text
-    } catch (error) {
-        return null
-    }
-}
+  try {
+    const html = await fetch(url, {
+      headers: {
+        "User-Agent": "Club Metatag API (https://club.yashstack.com)",
+      },
+    });
+    const text = await html.text();
+    return text;
+  } catch (error) {
+    return null;
+  }
+};
 
 export const getHeadTag = (html: string) => {
-    const parsedHTML = parse(html)
-    const metaTags = parsedHTML.querySelectorAll("meta").map(({ attributes }) => {
-        console.log('attributes.property',attributes.property || attributes.name || attributes.href, "and content: ", attributes.content)
-        return {
-            property: attributes.property || attributes.name || attributes.href,
-            content: attributes.content
-        }
-    })
-    const title = parsedHTML.querySelector("title")?.innerText
+  const parsedHTML = parse(html);
+  const metaTags = parsedHTML.querySelectorAll("meta").map(({ attributes }) => {
     return {
-        metaTags,
-        title
-    }
-    // console.log('parsed html : ', title);
-}
+      property: attributes.property || attributes.name || attributes.href,
+      content: attributes.content,
+    };
+  });
+  const title = parsedHTML.querySelector("title")?.innerText;
+  return {
+    metaTags,
+    title,
+  };
+  // console.log('parsed html : ', title);
+};
 
 export const getMetaTags = async (url: string) => {
-    console.log('url in metatag',url);
-    const html = await getHtmlfromURL(url);
-    if (!html) {
-        return {
-            title: null,
-            description: "No description",
-            image: null,
-        }
-    }
-    const { title: titleTag, metaTags } = getHeadTag(html)
-    const ogTitle = metaTags.find(tag => tag.property === 'og:title')?.content;
-    const ogDescription = metaTags.find(tag => tag.property === 'og:description')?.content;
-    const ogImage = metaTags.find(tag => tag.property === 'og:image')?.content;
-    const twitterImage = metaTags.find(tag => tag.property === 'twitter:image')?.content;
-    const imageSrc = metaTags.find(tag => tag.property === 'image_src')?.content;
-
-
-    // Fall back to description meta tag if og:description is not found
-    const description = ogDescription || metaTags.find(tag => tag.property === 'description')?.content || "No description";
-    console.log('Open graph data:', {
-        ogTitle,
-        ogDescription,
-        ogImage,
-        twitterImage,
-        imageSrc,
-        description
-    });
+  const html = await getHtmlfromURL(url);
+  if (!html) {
     return {
-        title: ogTitle || titleTag || url,
-        description,
-        image: ogImage || twitterImage || imageSrc || null
+      title: url,
+      description: "No description",
+      image: null,
     };
-}
+  }
+  const { metaTags, title: titleTag } = getHeadTag(html);
+
+  let object: { [key: string]: string | undefined } = {};
+
+  Object.entries(metaTags).forEach(([key, { property, content }]) => {
+    if (property && !object[property]) {
+      object[property] = content && he.decode(content);
+    }
+  });
+
+  console.log('object',object);
+
+  const title = object["og:title"] || object["twitter:title"] || titleTag;
+
+  const description =
+    object["description"] ||
+    object["og:description"] ||
+    object["twitter:description"];
+
+  const image =
+    object["og:image"] ||
+    object["twitter:image"] ||
+    object["image_src"] ||
+    object["icon"] ||
+    object["shortcut icon"];
+
+  return {
+    title: title || url,
+    description: description || "No description",
+    image,
+  };
+};
