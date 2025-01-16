ce94f78f5272cc675268eb9f120c063e3ef7e66c
yash-devop <kamble1234meena@gmail.com>
2025-01-16 20:42:26 +0530

    testing node-fetch

--- apps/web/app/api/metatags/route.ts
+++ apps/web/app/api/metatags/route.ts
@@ -1,58 +1,44 @@
 import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
-import og from "open-graph";
+import { getMetaTags } from "./utils";
+
 
 const CORS_HEADERS = {
-  "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Methods": "GET, OPTIONS",
-};
-
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET, OPTIONS",
+  };
+  
 const urlSearchParamsSchema = z.object({
-  url: z.string().refine(
-    (url) => {
-      console.log("url in zod : ", url);
-      try {
-        new URL(url);
-        return true;
-      } catch (error) {
-        return false;
-      }
-    },
-    {
-      message: "Invalid URL",
-    }
-  ),
-});
+    url: z.string().refine((url) => {
+        console.log('url in zod : ', url);
+        try {
+            new URL(url)
+            return true
+        } catch (error) {
+            return false
+        }
+    }, {
+        message: "Invalid URL"
+    })
+})
 
 export const GET = async (req: NextRequest) => {
-  const { url } = urlSearchParamsSchema.parse({
-    url: req.nextUrl.searchParams.get("url"),
-  });
+    const { url } = urlSearchParamsSchema.parse({
+        url: req.nextUrl.searchParams.get("url")
+    })
 
-  try {
-    const meta = await new Promise<any>((resolve, reject) => {
-      og(url, (err, meta) => {
-        if (err) reject(err);
-        else resolve(meta);
-      });
-    });
+    const metaTags = await getMetaTags(url)
 
-    return NextResponse.json(
-      {
-        title: meta?.title,
-        description: meta?.description,
-        image: meta?.image?.url,
-      },
-      { headers: CORS_HEADERS }
-    );
-  } catch (error) {
-    return NextResponse.json(
-      { error: "Failed to fetch metadata" },
-      { status: 500, headers: CORS_HEADERS }
-    );
-  }
-};
+    return NextResponse.json({
+        ...metaTags,
+    },{
+        headers: CORS_HEADERS
+    })
+}
 
 export function OPTIONS() {
-  return new Response(null, { status: 204, headers: CORS_HEADERS });
-}
+    return new Response(null, {
+      status: 204,
+      headers: CORS_HEADERS,
+    });
+  }
--- apps/web/app/api/metatags/utils.ts
+++ apps/web/app/api/metatags/utils.ts
@@ -3,11 +3,18 @@ import he from "he";
 
 export const getHtmlfromURL = async (url: string) => {
   try {
-    const html = await fetch(url, {
+    // const html = await fetch(url, {
+    //   headers: {
+    //     "User-Agent": "Club Metatag API (https://club.yashstack.com)",
+    //   },
+    // });
+    const options = {
+      method: "GET",
       headers: {
-        "User-Agent": "Club Metatag API (https://club.yashstack.com)",
+        "User-Agent":"Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36",
       },
-    });
+    };
+    const html = await fetch(url, options);
     const text = await html.text();
     return text;
   } catch (error) {
@@ -50,7 +57,7 @@ export const getMetaTags = async (url: string) => {
     }
   });
 
-  console.log('object',object);
+  console.log("object", object);
 
   const title = object["og:title"] || object["twitter:title"] || titleTag;
 
