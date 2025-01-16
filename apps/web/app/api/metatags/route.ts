09daaf6cba622739a19d5798eb2b502eb869e9bf
yash-devop <kamble1234meena@gmail.com>
2025-01-16 21:07:30 +0530

    open graph scraper package

--- apps/web/app/api/metatags/route.ts
+++ apps/web/app/api/metatags/route.ts
@@ -1,44 +1,57 @@
 import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
-import { getMetaTags } from "./utils";
-
+// import { getMetaTags } from "./utils";
+import ogs from "open-graph-scraper";
 
 const CORS_HEADERS = {
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "GET, OPTIONS",
-  };
-  
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET, OPTIONS",
+};
+
 const urlSearchParamsSchema = z.object({
-    url: z.string().refine((url) => {
-        console.log('url in zod : ', url);
-        try {
-            new URL(url)
-            return true
-        } catch (error) {
-            return false
-        }
-    }, {
-        message: "Invalid URL"
-    })
-})
+  url: z.string().refine(
+    (url) => {
+      console.log("url in zod : ", url);
+      try {
+        new URL(url);
+        return true;
+      } catch (error) {
+        return false;
+      }
+    },
+    {
+      message: "Invalid URL",
+    }
+  ),
+});
 
 export const GET = async (req: NextRequest) => {
-    const { url } = urlSearchParamsSchema.parse({
-        url: req.nextUrl.searchParams.get("url")
-    })
+  const { url } = urlSearchParamsSchema.parse({
+    url: req.nextUrl.searchParams.get("url"),
+  });
 
-    const metaTags = await getMetaTags(url)
+  const options = { url };
+  const {result , response} = await ogs(options)
 
-    return NextResponse.json({
-        ...metaTags,
-    },{
-        headers: CORS_HEADERS
-    })
-}
+  console.log('response in metatag',response);
+  return NextResponse.json({
+    title: result.ogTitle || "No title",
+    description: result.ogDescription || "No description",
+    image: result.ogImage?.[0]?.url || "No image"
+  })
+
+  // const metaTags = await getMetaTags(url)
+
+  // return NextResponse.json({
+  //     ...metaTags,
+  // },{
+  //     headers: CORS_HEADERS
+  // })
+};
 
 export function OPTIONS() {
-    return new Response(null, {
-      status: 204,
-      headers: CORS_HEADERS,
-    });
-  }
+  return new Response(null, {
+    status: 204,
+    headers: CORS_HEADERS,
+  });
+}
